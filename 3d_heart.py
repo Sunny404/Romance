7655f7ea5f38e6cf854db99f470951d1a16a24e9
sunny404 <mdanis6066@gmail.com>
2021-04-05 18:25:33 +0600

    Initial commit

---
+++ 3d_heart.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+color('pink')
+begin_fill()
+left(50)
+forward(100)
+circle(40, 180)
+left(260)
+circle(40, 180)
+forward(100)
+color('red')
+forward(10)
+left(100)
+forward(100)
+circle(47, 180)
+end_fill()
+
+done()
